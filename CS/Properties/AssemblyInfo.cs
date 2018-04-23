0294c99d3a3d66541caab0f2fb15aabb65bcf46c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿// Developer Express Code Central Example:
+// How to: Use the POCO Mechanism to implement the IDataErrorInfo Interface
+// 
+// This Code Example demonstrates how you use the POCO mechanism to implement the
+// IDataErrorInfo interface.
+// For more information about POCO, refer to the
+// following topic: DevExpress MVVM Framework. Introduction to POCO ViewModels.
+// (https://community.devexpress.com/blogs/wpf/archive/2013/12/04/devexpress-mvvm-framework-introduction-to-poco-viewmodels.aspx)
+// The
+// following article describes how to automatically generate an IDataErrorInfo
+// implementation: DevExpress MVVM Framework. Data validation. Implementing
+// IDataErrorInfo.
+// (https://community.devexpress.com/blogs/wpf/archive/2014/03/18/devexpress-mvvm-framework-data-validation-implementing-idataerrorinfo.aspx)
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E5151
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("ExampleMvvm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExampleMvvm")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿' Developer Express Code Central Example:
+' How to: Use the POCO Mechanism to implement the IDataErrorInfo Interface
+' 
+' This Code Example demonstrates how you use the POCO mechanism to implement the
+' IDataErrorInfo interface.
+' For more information about POCO, refer to the
+' following topic: DevExpress MVVM Framework. Introduction to POCO ViewModels.
+' (https://community.devexpress.com/blogs/wpf/archive/2013/12/04/devexpress-mvvm-framework-introduction-to-poco-viewmodels.aspx)
+' The
+' following article describes how to automatically generate an IDataErrorInfo
+' implementation: DevExpress MVVM Framework. Data validation. Implementing
+' IDataErrorInfo.
+' (https://community.devexpress.com/blogs/wpf/archive/2014/03/18/devexpress-mvvm-framework-data-validation-implementing-idataerrorinfo.aspx)
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E5151
+
+Imports System
+Imports System.Reflection
+Imports System.Resources
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+Imports System.Windows
+
+<Assembly: AssemblyTitle("ExampleMvvm")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ExampleMvvm")>
+<Assembly: AssemblyCopyright("")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+<Assembly: CLSCompliant(True)>
+<Assembly: ComVisible(False)>
+<Assembly: NeutralResourcesLanguage("en-US")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
