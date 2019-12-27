4a313220483c35b8917503ad7a07da2c2335af78
DevExpressExampleBot <fakeEmail>
2019-12-27 12:53:38 +0300

    autogenerate VB

--- VB/View/MainView.xaml.vb
+++ VB/View/MainView.xaml.vb
@@ -18,11 +18,11 @@
 Imports System.Windows.Controls
 
 Namespace Example.View
-    Partial Public Class MainView
-        Inherits UserControl
+	Partial Public Class MainView
+		Inherits UserControl
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
 End Namespace
