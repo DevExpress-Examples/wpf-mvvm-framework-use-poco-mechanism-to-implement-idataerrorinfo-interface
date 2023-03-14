f1eac075920e7b99cf5f8e000ed99ab78413efff
AndreySlabov <andrey.slabov@devexpress.com>
2023-03-14 13:55:37 +0400

    format

--- CS/View/MainView.xaml.cs
+++ CS/View/MainView.xaml.cs
@@ -1,21 +1,4 @@
-﻿// Developer Express Code Central Example:
-// How to: Use the POCO Mechanism to implement the IDataErrorInfo Interface
-// 
-// This Code Example demonstrates how you use the POCO mechanism to implement the
-// IDataErrorInfo interface.
-// For more information about POCO, refer to the
-// following topic: DevExpress MVVM Framework. Introduction to POCO ViewModels.
-// (https://community.devexpress.com/blogs/wpf/archive/2013/12/04/devexpress-mvvm-framework-introduction-to-poco-viewmodels.aspx)
-// The
-// following article describes how to automatically generate an IDataErrorInfo
-// implementation: DevExpress MVVM Framework. Data validation. Implementing
-// IDataErrorInfo.
-// (https://community.devexpress.com/blogs/wpf/archive/2014/03/18/devexpress-mvvm-framework-data-validation-implementing-idataerrorinfo.aspx)
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E5151
-
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace Example.View {
     public partial class MainView : UserControl {
